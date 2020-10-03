9a59e7a4ccf9262c8368ac30b6f21e070a2d82fc
Andre Serudo <andreserudo@gmail.com>
2020-10-03 18:40:15 -0400

    creating dynamic html color elements

--- src/script.js
+++ src/script.js
@@ -1,19 +1,36 @@
+const btnStart = document.getElementById('startGame');
+let currentLevel = 0;
+const game = [
+  {
+    level: 1,
+    phases: 3,
+    colors: 3,
+    givenCollors: ['red', 'blue'],
+    colorOne: ['rgb()','rgb()'],
+    colorTwo: ['rgb()','rgb()'],
+  }
+];    
+
+function createColor(){    
+  const colors = document.querySelector('.colors');  
+  let color = document.createElement('div');
+
+  color.className = 'color';
+  //console.log(color);
+  colors.append(color);
+}
 
 function newGame(){ 
   const initialScreen = document.querySelector('.initial-screen');
   const gameScreen = document.querySelector('.game');
   
   initialScreen.style.display = 'none';  
-  gameScreen.style.display = 'flex';
+  gameScreen.style.display = 'flex';  
+  console.log('olar');
 
-  loadGame();
-  
+  for(let i=0; i<game[currentLevel].colors; i+= 1){
+    createColor();
+  }
 }
 
-function loadGame(){
-  const btnStart = document.getElementById('startGame');
-
-  btnStart.addEventListener('click', newGame)
-}
-
-window.onload = loadGame();
+btnStart.addEventListener('click', newGame)
