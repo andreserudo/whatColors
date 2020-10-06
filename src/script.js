889312f27979e9f4350eb72280ec31ce4db4c431
Andre Serudo <andreserudo@gmail.com>
2020-10-06 10:34:39 -0400

    making colors been randomly chosen

--- src/script.js
+++ src/script.js
@@ -1,15 +1,57 @@
 const btnStart = document.getElementById('startGame');
 let currentLevel = 0;
+let currentPhase = 0;
 const game = [
   {
     level: 1,
     phases: 3,
     colors: 3,
-    givenCollors: ['red', 'blue'],
-    colorOne: ['rgb()','rgb()'],
-    colorTwo: ['rgb()','rgb()'],
+    givenCollors: ['rgb(255, 128, 128)', 'blue'],
+    colorOne: ['rgb(255, 0, 0)','rgb()'],
+    colorTwo: ['rgb(255, 255, 255)','rgb()'],
+    colorRandom: ['rgb(255, 128, 213)','']
   }
 ];    
+
+// An implementation of Fisher-Yates (aka Knuth) Shuffle algorithm
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function stylingColor(level, phase){
+  let numberOfRandomColors = game.colors - 2;  
+  let arrayColors = [ 
+    game[currentLevel].colorOne[currentPhase], 
+    game[currentLevel].colorTwo[currentPhase], 
+    game[currentLevel].colorRandom[currentPhase]
+  ];
+  const colors = document.querySelectorAll('.colors .color');
+  let index = 0;
+
+  arrayColors = shuffle(arrayColors);
+  
+  colors.forEach(element => {
+    element.style.backgroundColor = arrayColors[index];
+    index += 1;
+  })
+  console.log(colors);
+}
 
 function createColor(){    
   const colors = document.querySelector('.colors');  
@@ -26,11 +68,14 @@ function newGame(){
   
   initialScreen.style.display = 'none';  
   gameScreen.style.display = 'flex';  
-  console.log('olar');
+  console.log(game[currentLevel].givenCollors[currentPhase]);
 
   for(let i=0; i<game[currentLevel].colors; i+= 1){
     createColor();
   }
+
+  stylingColor();
+  //stylingColor(currentLevel, currentPhase);
 }
 
 btnStart.addEventListener('click', newGame)
