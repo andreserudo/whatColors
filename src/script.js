18dffd4ebfb869af7413a9db63790970db727ce0
Andre Serudo <andreserudo@gmail.com>
2020-10-06 22:07:09 -0400

    passing phase, changing chosen color

--- src/script.js
+++ src/script.js
@@ -1,17 +1,30 @@
 const btnStart = document.getElementById('startGame');
 let currentLevel = 0;
 let currentPhase = 0;
+let tries = 3;
 const game = [
   {
     level: 1,
     phases: 3,
+    numberOfColorsToPass: 2,
     colors: 3,
-    givenCollors: ['rgb(255, 128, 128)', 'blue'],
-    colorOne: ['rgb(255, 0, 0)','rgb()'],
-    colorTwo: ['rgb(255, 255, 255)','rgb()'],
-    colorRandom: ['rgb(255, 128, 213)','']
+    givenCollors: ['rgb(255, 128, 128)', 'rgb(128, 36, 86)'],
+    colorOne: ['rgb(255, 0, 0)','rgb(255, 0, 0)'],
+    colorTwo: ['rgb(255, 255, 255)','rgb(0, 71, 171)'],
+    colorRandom: ['rgb(255, 128, 213)','rgb(0, 237, 255)']
   }
 ];    
+
+function loseTries() {
+  tries -= 1;
+
+  if (tries === 0) {
+    console.log('game over');
+  } else {
+    console.log(tries);
+  }
+  
+}
 
 // An implementation of Fisher-Yates (aka Knuth) Shuffle algorithm
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -34,34 +47,70 @@ function shuffle(array) {
   return array;
 }
 
-function stylingColor(level, phase){
-  let numberOfRandomColors = game.colors - 2;  
+function stylingColor(level, phase){  
   let arrayColors = [ 
     game[currentLevel].colorOne[currentPhase], 
     game[currentLevel].colorTwo[currentPhase], 
     game[currentLevel].colorRandom[currentPhase]
   ];
   const colors = document.querySelectorAll('.colors .color');
+  const givenColor = document.querySelector('.given-color');
   let index = 0;
+
+  givenColor.style.backgroundColor = game[currentLevel].givenCollors[currentPhase];
 
   arrayColors = shuffle(arrayColors);
   
   colors.forEach(element => {
     element.style.backgroundColor = arrayColors[index];
     index += 1;
-  })
-  console.log(colors);
+  });
+  
 }
 
-function choseColor(e){
-  console.log(e.srcElement);
+function chosenColour(e){
+  const colourElement = e.srcElement;
+  const colourChoose = e.srcElement.style.backgroundColor;
+  const colourClassName = e.srcElement.className;
+
+  if (!colourClassName.includes('correct')) {
+    if((game[currentLevel].colorOne[currentPhase] === colourChoose) || 
+      (game[currentLevel].colorTwo[currentPhase] === colourChoose) ){
+        const otherIncorrect = document.querySelector('.incorrect');
+        console.log(otherIncorrect);
+        if(otherIncorrect){
+          otherIncorrect.classList.remove('incorrect');
+        }        
+        colourElement.className = 'color correct';
+    }else{
+        if (colourElement.className != 'color incorrect') {          
+          colourElement.className = 'color incorrect';
+          loseTries();  
+        }
+    }    
+  }
+
+  const totalAnswers = document.querySelectorAll('.correct');  
+
+  if (totalAnswers.length === game[currentLevel].numberOfColorsToPass) {
+    newPhase();
+  }
+  
 }
 
 function addingClickColorEvent() {
   const colors = document.querySelectorAll('.colors .color');
 
   colors.forEach(element => {
-    element.addEventListener('click', choseColor);
+    element.addEventListener('click', chosenColour);
+  })
+}
+
+function removingColors() {
+  const colors = document.querySelectorAll('.colors .color');
+
+  colors.forEach(element => {
+    element.remove();
   })
 }
 
@@ -70,8 +119,20 @@ function createColor(){
   let color = document.createElement('div');
 
   color.className = 'color';
-  //console.log(color);
   colors.append(color);
+}
+
+function newPhase(){
+  currentPhase++;
+
+  removingColors();
+
+  for(let i=0; i<game[currentLevel].colors; i+= 1){
+    createColor();
+    addingClickColorEvent()
+  }
+
+  stylingColor();  
 }
 
 function newGame(){ 
@@ -88,7 +149,7 @@ function newGame(){
   }
 
   stylingColor();
-  //stylingColor(currentLevel, currentPhase);
+  
 }
 
 btnStart.addEventListener('click', newGame)
